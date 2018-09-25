254e8717bf66eda16b036fc740afa6f22fa5f9ea
nostprumprajee <nostprumprajee@gmail.com>
2018-09-25 16:12:01 +0700

    MongoDB

---
+++ demo-mongodb/src/main/java/com/example/demomongodb/DemoMongodbApplication.java
@@ -0,0 +1,12 @@
+package com.example.demomongodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoMongodbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoMongodbApplication.class, args);
+	}
+}
---
+++ demo-mongodb/src/test/java/com/example/demomongodb/DemoMongodbApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.demomongodb;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DemoMongodbApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
