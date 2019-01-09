174606ba4af05fb631d8e76b7c8606250965204b
nostprumprajee <nostprumprajee@gmail.com>
2019-01-09 15:54:03 +0700

    grpc

---
+++ shippy-tutorial-2/README.md
@@ -0,0 +1,17 @@
+# Shippy
+
+This is the codebase for the multi-part series on writing microservices in Golang [found here](ewanvalentine.io).
+
+
+## Table of contents (this may change as the series progresses).
+
+1. Tutorial one - `$ git checkout tutorial-1` - Microservice basics, gRPC and protobuf basics.
+2. Tutorial two - `$ git checkout tutorial-2` - Docker basics, creating a second service, integrating go-micro.
+3. Tutorial three (TODO) - Creating a third service. Introducing docker-compose. Introducing Mongodb and mgo, adding a datastore.
+4. Tutorial four (TODO) - Introducing NATS and message brokering.
+5. Tutorial five (TODO) - Creating a user-interface. Introducing gRPC-gateway for exposing RESTful endpoints for web.
+6. Tutorial six (TODO) - Deploying to Google Cloud part 1 - introducing Terraform and Google Cloud.
+7. Tutorial seven (TODO) - Deploying to Google Container Engine - introducing Container Engine and Kubernetes.
+8. Tutorial eight (TODO) - Setting up continuous integration with CircleCI.
+9. Tutorial nine (TODO) - API gateway and routing. Introducing Kong.
+10. Tutorial ten (TODO) - Conclusion.
---
+++ shippy-tutorial-2/consignment-service/main.go
@@ -0,0 +1,108 @@
+package main
+
+import (
+
+	// Import the generated protobuf code
+	"fmt"
+	"log"
+
+	pb "github.com/EwanValentine/shippy/consignment-service/proto/consignment"
+	vesselProto "github.com/EwanValentine/shippy/vessel-service/proto/vessel"
+	micro "github.com/micro/go-micro"
+	"golang.org/x/net/context"
+)
+
+type Repository interface {
+	Create(*pb.Consignment) (*pb.Consignment, error)
+	GetAll() []*pb.Consignment
+}
+
+// Repository - Dummy repository, this simulates the use of a datastore
+// of some kind. We'll replace this with a real implementation later on.
+type ConsignmentRepository struct {
+	consignments []*pb.Consignment
+}
+
+func (repo *ConsignmentRepository) Create(consignment *pb.Consignment) (*pb.Consignment, error) {
+	updated := append(repo.consignments, consignment)
+	repo.consignments = updated
+	return consignment, nil
+}
+
+func (repo *ConsignmentRepository) GetAll() []*pb.Consignment {
+	return repo.consignments
+}
+
+// Service should implement all of the methods to satisfy the service
+// we defined in our protobuf definition. You can check the interface
+// in the generated code itself for the exact method signatures etc
+// to give you a better idea.
+type service struct {
+	repo Repository
+	vesselClient vesselProto.VesselServiceClient
+}
+
+// CreateConsignment - we created just one method on our service,
+// which is a create method, which takes a context and a request as an
+// argument, these are handled by the gRPC server.
+func (s *service) CreateConsignment(ctx context.Context, req *pb.Consignment, res *pb.Response) error {
+
+	// Here we call a client instance of our vessel service with our consignment weight,
+	// and the amount of containers as the capacity value
+	vesselResponse, err := s.vesselClient.FindAvailable(context.Background(), &vesselProto.Specification{
+		MaxWeight: req.Weight,
+		Capacity: int32(len(req.Containers)),
+	})
+	log.Printf("Found vessel: %s \n", vesselResponse.Vessel.Name)
+	if err != nil {
+		return err
+	}
+
+	// We set the VesselId as the vessel we got back from our
+	// vessel service
+	req.VesselId = vesselResponse.Vessel.Id
+
+	// Save our consignment
+	consignment, err := s.repo.Create(req)
+	if err != nil {
+		return err
+	}
+
+	// Return matching the `Response` message we created in our
+	// protobuf definition.
+	res.Created = true
+	res.Consignment = consignment
+	return nil
+}
+
+func (s *service) GetConsignments(ctx context.Context, req *pb.GetRequest, res *pb.Response) error {
+	consignments := s.repo.GetAll()
+	res.Consignments = consignments
+	return nil
+}
+
+func main() {
+
+	repo := &ConsignmentRepository{}
+
+	// Create a new service. Optionally include some options here.
+	srv := micro.NewService(
+
+		// This name must match the package name given in your protobuf definition
+		micro.Name("go.micro.srv.consignment"),
+		micro.Version("latest"),
+	)
+
+	vesselClient := vesselProto.NewVesselServiceClient("go.micro.srv.vessel", srv.Client())
+
+	// Init will parse the command line flags.
+	srv.Init()
+
+	// Register handler
+	pb.RegisterShippingServiceHandler(srv.Server(), &service{repo, vesselClient})
+
+	// Run the server
+	if err := srv.Run(); err != nil {
+		fmt.Println(err)
+	}
+}
